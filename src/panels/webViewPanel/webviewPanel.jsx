c0b6dc7e34c5cc314a82feb89f357a10e63e4185
Kishikawa1286 <k.ishikawa.gude@gmail.com>
2020-07-29 17:52:13 +0900

    webviewに戻る進むを実装

--- src/panels/webViewPanel/webviewPanel.jsx
+++ src/panels/webViewPanel/webviewPanel.jsx
@@ -1,41 +1,63 @@
 import React from "react";
 
-const WebViewPanel = (props) => {
+const WebViewAppBarButton = (props) => {
   // eslint-disable-next-line react/prop-types
-  const { height = 200, src = "https://google.com", deleteWebViewPanel } = props;
+  const { text, onClick } = props;
   return (
-    <div>
-      <div
-        style={{
-          width: "100%",
-          height: "36px",
-          margin: 0,
-          padding: 0,
-          background: "rgba(0, 0, 0, 0.5)",
-        }}
-      >
-        <button
-          type="button"
-          onClick={deleteWebViewPanel}
-          style={{
-            margin: 0,
-            padding: "0 0 0 16px",
-            border: "none",
-            outline: 0,
-            background: "rgba(0, 0, 0, 0)",
-            color: "#FFFFFF",
-            fontSize: "30px",
-            textAlign: "center",
-          }}
-        >
-          ✕
-        </button>
-      </div>
-      <div style={{ width: "100%", height: `${height}px` }}>
-        <webview src={src} height={height.toString()} style={{ minHeight: `${height}px` }} />
-      </div>
-    </div>
+    <button
+      type="button"
+      onClick={onClick}
+      style={{
+        margin: 0,
+        padding: "0 0 0 16px",
+        border: "none",
+        outline: 0,
+        background: "rgba(0, 0, 0, 0)",
+        color: "#FFFFFF",
+        fontSize: "30px",
+        textAlign: "center",
+      }}
+    >
+      {text}
+    </button>
   );
 };
+
+class WebViewPanel extends React.Component {
+  constructor() {
+    super();
+    this.webViewRef = undefined;
+  }
+
+  render() {
+  // eslint-disable-next-line react/prop-types
+    const { height = 200, src = "https://google.com", deleteWebViewPanel } = this.props;
+    return (
+      <div>
+        <div
+          style={{
+            width: "100%",
+            height: "36px",
+            margin: 0,
+            padding: 0,
+            background: "rgba(0, 0, 0, 0.5)",
+          }}
+        >
+          <WebViewAppBarButton text="✕" onClick={deleteWebViewPanel} />
+          <WebViewAppBarButton text="<" onClick={() => this.webViewRef.goBack()} />
+          <WebViewAppBarButton text=">" onClick={() => this.webViewRef.goForward()} />
+        </div>
+        <div style={{ width: "100%", height: `${height}px` }}>
+          <webview
+            src={src}
+            height={height.toString()}
+            style={{ minHeight: `${height}px` }}
+            ref={(webview) => { this.webViewRef = webview; }}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default WebViewPanel;
